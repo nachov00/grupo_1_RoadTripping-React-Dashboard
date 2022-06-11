8e87395f48c17eb7f5fe7b603e9f33bf0dd0eb07
Lis Perdomo <ing.lisperdomo@gmail.com>
2022-06-10 23:53:31 -0300

    vista UI kit

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -3,55 +3,70 @@ import Card from "./Card";
 import "../assets/App.css";
 import "../assets/css/DashBoard.css";
 import Logo from "../assets/images/Logo.png";
-import Test from "../assets/images/Test1.jpg"
-import image1 from "../assets/images/mapamundi-politico.png";
+import Test from "../assets/images/Test1.jpg";
+import image1 from "../assets/images/MapaMundi.png";
+import image2 from "../assets/images/VentasTemporadas.png";
+import image3 from "../assets/images/VentasMensual.png";
+import image4 from "../assets/images/Usuarios.png";
+import image5 from "../assets/images/BalanceAnual.png";
+import image6 from "../assets/images/Glaciar.jpg";
+import image7 from "../assets/images/Mendoza.jpg";
+
 
 function DashBoard() {
 
-  
+
   return (
     <React.Fragment>
       <div id="Dashboard">
         <div id="Container-Superior">
 
-            <div id="Container-S-Izquierdo">
-            
+          <div id="Container-S-Izquierdo">
+
             <div id="Container-S-I-S">
-            <img src={Logo} className="mine-logo"/>
+              <img src={Logo} className="mine-logo" />
 
             </div>
             <div id="Container-S-I-I">
-            <Card image={Test} encabezado="Hola" valor="10"/>
-                
+              <div>
+                <Card image={image2} encabezado="Hola" valor="10" />
+                </div>
+              <div>
+                <Card image={image3} encabezado="De nuevo" valor="11" />
+                </div>
             </div>
 
-            </div>
+          </div>
 
-            <div id="Container-S-Derecho">
-            <Card image={image1} encabezado="Hola" valor="39"/>
-
-            </div>
+          <div id="Container-S-Derecho">
+            <Card image={image1} encabezado="Hola" valor="12" />
+          </div>
 
         </div>
         <div id="Container-Inferior">
 
-            
-            <div id="Container-I-Izquierdo">
-            
+          <div id="Container-I-Izquierdo">
+
             <div id="Container-I-I-Izquierdo">
-                <Card image={Logo} encabezado="Hola"/>
+              <div>
+                <Card image={image6} encabezado="Hola" />
+              </div>
+              <div>
+                <Card image={image7} encabezado="Hola" />
+              </div>
             </div>
+
             <div id="Container-I-I-Derecho">
-                <Card image={Test} encabezado="Hola"/>
+              <Card image={image4} encabezado="Hola" />
             </div>
 
-            </div>
+          </div>
 
-            <div id="Container-I-Derecho">
-            <Card image={Test} encabezado="Hola"/>
+          <div id="Container-I-Derecho">
+            <Card image={image5} encabezado="Hola" />
 
-            </div>
-            
+          </div>
+
         </div>
       </div>
     </React.Fragment>
